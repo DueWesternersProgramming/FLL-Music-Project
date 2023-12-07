2ddd6a289dd98e1ce2cff762d489c668fe714064
hjmcphail76 <126906797+hjmcphail76@users.noreply.github.com>
2023-12-07 15:38:41 -0500

    Updated and worked on GUI visuals. Added custom Tkinter.

--- GUI.py
+++ GUI.py
@@ -1,50 +1,49 @@
-import tkinter
+import customtkinter as ct
 import time
-#from volumemanagement import *
+import tkinter
 
-timerWindow = tkinter.Tk()
+#customtkinter.set_appearance_mode("dark")
+#print(base.test())
+
+timerWindow = ct.CTk()
 timerWindow.geometry("300x250")
 timerWindow.title("Timer Window")
 
-controller = tkinter.Tk()
+controller = ct.CTk()
 controller.title("Controller Window")
 controller.geometry("300x250")
-controller.config(bg="grey")
+
+startButton = ct.CTkButton(controller, text="Start Timer",
+command=lambda: startTimer(), 
+width=10, height= 3, font=("Serif", 20))
+
+startButton.pack(side=("left"))
+
+timer = ct.CTkLabel(timerWindow,text=("2:30"))
+timer.configure(font=("Serif", 180))
+timer.pack(side="top", pady=(timerWindow.winfo_height()/2))
 
 
-startButton = tkinter.Button(controller, text="Start Timer", command=lambda: startTimer(), width=10, height= 3, bg="blue", font=("Serif", 20))
 
-x = tkinter.Label(text=("2:30"))
-x.config(font=("Serif", 120))
-x.pack()
 
-x.place(
-    x=timerWindow.winfo_screenwidth() / 2-115, #Offset b/c of font size.
-    y=timerWindow.winfo_screenheight() / 2 - 200  #Offset b/c of font size.
-)
-
-startButton.place(
-    x = (timerWindow.winfo_screenwidth()/2)-80,
-    y = (timerWindow.winfo_screenheight()/2)-160
-)
 
 def startTimer():
   min = 2
   sec = 30
-  interval = 1
+  countInterval = 1
   while min >= 0:
     count = str(min) + ":" + str(sec)
     if sec < 1:
       sec = 60
       min = min - 1
     sec = sec-1
-    x.config(text=(str(count)))
+    timer.configure(text=(str(count)))
     timerWindow.update()
-    time.sleep(interval)
+    time.sleep(countInterval)
   time.sleep(3)
   min = 2
   sec = 30
   count = str(min) + ":" + str(sec)
-  x.config(text=str(count))
-print(x.winfo_width())
+  timer.configure(text=str(count))
+#print(timer.winfo_width())
 timerWindow.mainloop()
