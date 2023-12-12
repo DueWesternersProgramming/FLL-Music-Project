50648c12a620f1a41b5acf3f6b113df2bf007a11
Harrison McPhail <hjmcphail76@gmail.com>
2023-12-12 18:43:59 -0500

    Patched GUI placement bug

--- GUI.py
+++ GUI.py
@@ -16,19 +16,19 @@ def runGUI():
 
   startButton = ct.CTkButton(controller, text="Start Timer",
   command=lambda: Timer.startTimer(timer), 
-  width=10, height= 3, font=("Serif", 20))
+  width=13, height= 3, font=("Serif", 20))
 
   volumeUpButton = ct.CTkButton(controller, text="Volume Up",
   command=lambda: vm.volumeControl(True, controller),
-  width=10, height=3, font=("Serif", 20))
+  width=13, height=3, font=("Serif", 20))
 
   volumeDownButton = ct.CTkButton(controller, text="Volume Down",
   command=lambda: vm.volumeControl(False, controller),
-  width=10, height=3, font=("Serif", 20))
+  width=13, height=3, font=("Serif", 20))
 
-  startButton.pack(side=("left"), fill='y')
-  volumeUpButton.pack(side=("left"), fill='y')
-  volumeDownButton.pack(side=("left"), fill='y')
+  startButton.place(relx=0.5,rely=0.5)
+  volumeDownButton.place(relx=0.5,rely=0.6)
+  volumeUpButton.place(relx=0.5,rely=0.7)
 
   timer = ct.CTkLabel(timerWindow,text=("2:30"))
   timer.configure(font=("Serif", 180))
@@ -37,7 +37,7 @@ def runGUI():
   sheet.initEntrySheet(timerWindow)
   sheet.initDisplaySheet(controller)
   
-  print(sheet.getData())
+  #print(sheet.getData())
 
 
   timerWindow.mainloop()
