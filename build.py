c7509f83e41d4bb60c7c12ef1cfdf78c7a1fd6ab
Joel Logan <76826688+JoelLogan@users.noreply.github.com>
2023-12-16 11:18:22 -0500

    fixing names

---
+++ build.py
@@ -0,0 +1,21 @@
+"""Module to run PyInstaller to build the project"""
+import platform
+import PyInstaller.__main__
+
+LOCATION = ""
+system = platform.system()
+
+if system == "Windows":
+    LOCATION = r'.\fll_presenter\Main.py'
+    PyInstaller.__main__.run([
+    LOCATION,
+    #'--onefile',
+    '--windowed'
+    ])
+
+elif system == "Linux":
+    LOCATION = './fll_presenter/Main.py'
+    PyInstaller.__main__.run([
+    LOCATION
+    #,'--onefile'
+    ])
