a018cfb135d2b7c4e9634dff4188de5ab4f1440d
Joel Logan <76826688+JoelLogan@users.noreply.github.com>
2023-12-20 17:23:39 -0500

    Trying Shiv

--- fll_presenter/main.py
+++ fll_presenter/main.py
@@ -1,4 +1,9 @@
 """Main module that starts everything"""
 import config_gui
 
-config_gui.run_gui()
+def main():
+    """Function to start FLL Presenter"""
+    config_gui.run_gui()
+
+if __name__ == "__main__":
+    main()
